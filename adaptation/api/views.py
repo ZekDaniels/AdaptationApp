22b49d12a447868a0eb61772a71ab26fc0c434b3
ZekDaniels <zekavsrdev@gmail.com>
2022-01-25 04:49:42 +0300

    #55 fix: filter working, filter used

--- adaptation/api/views.py
+++ adaptation/api/views.py
@@ -1,36 +1,35 @@
 from adaptation.api.serializers import *
 from django.core.validators import EMPTY_VALUES
-from django.db.models import QuerySet
+from django.db.models import QuerySet, ForeignObjectRel, ForeignKey, Q
 from rest_framework import generics
 from rest_framework.filters import OrderingFilter, SearchFilter
-from rest_framework.views import APIView
-from rest_framework_datatables.filters import DatatablesFilterBackend
+from rest_framework_datatables.filters import DatatablesFilterBackend, f_search_q
 
 from adaptation.models import Faculty
+
+class CustomDatatablesFilterBackend(DatatablesFilterBackend):
+
+    def get_q(self, datatables_query):
+        q = Q()
+        initial_q = Q()
+        for f in datatables_query['fields']:
+            if not f['searchable']:
+                continue
+            q |= f_search_q(f,
+                            datatables_query['search_value'],
+                            datatables_query['search_regex'])
+            initial_q &= f_search_q(f,
+                                    f.get('search_value'),
+                                    f.get('search_regex', False))
+        q &= initial_q
+        return q
 
 class QueryListAPIView(generics.ListAPIView):
     def get_queryset(self):
         if self.request.GET.get('format', None) == 'datatables':
-            self.filter_backends = (OrderingFilter, DatatablesFilterBackend)
+            self.filter_backends = (OrderingFilter, CustomDatatablesFilterBackend)
             return super().get_queryset()
         queryset = self.queryset
-
-        # check the start index is integer
-        try:
-            start = self.request.GET.get('start')
-            start = int(start) if start else None
-        # else make it None
-        except ValueError:
-            start = None
-
-        # check the end index is integer
-        try:
-            end = self.request.GET.get('end')
-            end = int(end) if end else None
-        # else make it None
-        except ValueError:
-            end = None
-
         # skip filters and sorting if they are not exists in the model to ensure security
         accepted_filters = {}
         # loop fields of the model
@@ -38,10 +37,13 @@ class QueryListAPIView(generics.ListAPIView):
             # if field exists in request, accept it
             if field.name in dict(self.request.GET):
                 accepted_filters[field.name] = dict(self.request.GET)[field.name]
-            # if field exists in sorting parameter's value, accept it
-
+            if isinstance(field, ForeignObjectRel) or isinstance(field, ForeignKey):
+                for n in field.related_model._meta.get_fields():
+                    related_field = f"{field.name}__{n.name}"
+                    if related_field in dict(self.request.GET):
+                        accepted_filters[related_field] = dict(self.request.GET)[related_field]
+                        dict(self.request.GET)
         filters = {}
-
         for key, value in accepted_filters.items():
             if any(val in value for val in EMPTY_VALUES):
                 if queryset.model._meta.get_field(key).null:
@@ -52,7 +54,7 @@ class QueryListAPIView(generics.ListAPIView):
                 filters[key + '__in'] = value
         if isinstance(queryset, QuerySet):
             # Ensure queryset is re-evaluated on each request.
-            queryset = queryset.all().filter(**filters)[start:end]
+            queryset = queryset.filter(**filters)
         return queryset
 
     @property
@@ -68,6 +70,7 @@ class QueryListAPIView(generics.ListAPIView):
             else:
                 self._paginator = None
         return self._paginator
+
 
 class FacultyListView(QueryListAPIView):
     
@@ -105,10 +108,11 @@ class AdaptationClassDetailAPIView(generics.RetrieveAPIView):
     
 class StudentClassListAPIView(QueryListAPIView):
    
-    custom_related_fields = ["adaptation_class","adaptation"]
+    custom_related_fields = ["adaptation", "adaptation_class"]
     queryset = StudentClass.objects.select_related(*custom_related_fields).all().order_by('-adaptation_class__pk') 
     serializer_class = StudentClassListSerializer
     filter_backends = [OrderingFilter, SearchFilter]
+
     ordering_fields = "__all__"
     search_fields = ['code', 'class_name','adaptation_class__code', 'adaptation_class__class_name']
 
--- core/settings/base.py
+++ core/settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
+    'django_cleanup.apps.CleanupConfig',
     #myapps
     'user',
     'adaptation',
