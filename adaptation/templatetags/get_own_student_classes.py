e71a367905a4ec16eee56ac6990e1629f08eb529
ZekDaniels <zekavsrdev@gmail.com>
2022-02-03 14:25:10 +0300

    #90 fix: branch error fixed

--- adaptation/templatetags/get_own_student_classes.py
+++ adaptation/templatetags/get_own_student_classes.py
@@ -3,4 +3,9 @@ register = template.Library()
 
 @register.filter
 def get_own_student_classes(obj, adaptation):
+   return obj.get_own_student_classes(adaptation).order_by("adaptation_class")
+
+
+@register.filter
+def get_own_student_classes_count(obj, adaptation):
    return obj.get_own_student_classes(adaptation).count()
