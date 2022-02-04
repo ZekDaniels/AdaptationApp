f4cae576489d30927902054b96d12eef0109320c
ZekDaniels <zekavsrdev@gmail.com>
2022-02-04 08:30:18 +0300

    #100 refact(): admin adaptation filter added

--- adaptation/admin.py
+++ adaptation/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from adaptation.models import *
 # Register your models here.
+class AdaptationAdmin(admin.ModelAdmin):
+    search_fields = ['user__profile__student_number','user__profile__namesurname']
+
+
 admin.site.register(University)
 admin.site.register(Faculty)
 admin.site.register(Science)
-admin.site.register(Adaptation)
+admin.site.register(Adaptation, AdaptationAdmin)
 admin.site.register(StudentClass)
 admin.site.register(AdapatationClass)
 admin.site.register(AdaptationClassConfirmation)
