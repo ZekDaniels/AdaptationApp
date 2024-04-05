15c59b0bb8c5f7b577b654330cc5dce85e8b345e
Zekeriya Avsar <44510178+ZekDaniels@users.noreply.github.com>
2024-04-06 02:28:27 +0300

    Update production.py

--- core/settings/production.py
+++ core/settings/production.py
@@ -11,7 +11,7 @@ DATABASES = {
         ),
         "NAME": os.environ.get("SQL_DATABASE", "intibak"),
         "USER": os.environ.get("SQL_USER", "postgres"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "Novu.2021"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
     }
